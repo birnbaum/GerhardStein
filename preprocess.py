2f5a50b12b0859fa23086ba7783ee479beb5875e
Philipp Wiesner <mail@philippwiesner.com>
2017-12-01 02:02:36 +0100

    Improved SQL query for data set generator and fixed encoding issues

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import mysql.connector
 import progressbar
 import argparse
 import yaml
+import re
 
 
 def main():
@@ -13,9 +14,10 @@ def main():
     with open('config.yml', 'r') as c:
         config = yaml.load(c)
 
-    preprocess(args, config)
+    generate_text(args, config)
 
-def iter_row(cursor, size=100):
+
+def iter_row(cursor, size=1000):
     while True:
         rows = cursor.fetchmany(size)
         if not rows:
@@ -23,7 +25,8 @@ def iter_row(cursor, size=100):
         for row in rows:
             yield row
 
-def preprocess(args, config):
+
+def generate_text(args, config):
     cnx = mysql.connector.connect(user=config["database"]["user"],
                                   password=config["database"]["password"],
                                   host=config["database"]["host"],
@@ -34,17 +37,31 @@ def preprocess(args, config):
     count = cursor.fetchone()[0]
     bar = progressbar.ProgressBar(max_value=count)
 
-    cursor.execute('SELECT message FROM comment ORDER BY RAND()')
+    cursor.execute('''
+        SELECT p.message,
+            p.created_time as primary_sort,
+            Null as secondary_sort
+        FROM comment p
+        WHERE p.parent_comment IS NULL
+        UNION
+        SELECT c.message,
+            p.created_time as primary_sort,
+            c.created_time as secondary_sort
+        FROM comment c
+        JOIN comment p on p.id = c.parent_comment
+        ORDER BY primary_sort ASC,
+            secondary_sort ASC
+    ''')
     txt = ''
-    for (message,) in bar(iter_row(cursor)):
-        parts = message.replace('\r', '\n').split('\n')
-        for part in parts:
-            if 4 < len(part) < 1000:
-                txt += '> {}\n'.format(part.strip())
+    for (message, _, _) in bar(iter_row(cursor)):
+        lines = message.replace('\r', '\n').split('\n')
+        for line in lines:
+            if 4 < len(line) < 1000 and '@ ' not in line:
+                txt += '> {}\n'.format(line.strip())
 
     with open(args.out, "wb") as f:
-        f.write(txt.encode('utf8'))
+        f.write(txt.encode("cp1252", errors="ignore"))
 
 
 if __name__ == '__main__':
-	main()
+    main()
